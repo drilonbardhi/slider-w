48b4518b8c20f3e284d2c0f38d1c88ce6ccb1eeb
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-11-10 14:57:59 +0000

    added content for pages

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
+// data for holding which page we are on
 let pageNumber = 0
+
+// content for pages
+const pages = [
+  'a Londoner',
+  'a webflow developer',
+  'a JS developer',
+  'looking for a job',
+  'positive!'
+]
 
 const nextTag = document.querySelector("div img.is-next")
 const prevTag = document.querySelector("div img.is-previous")
 const outputTag = document.querySelector("p")
 
+// function to increase page number
 const next = function () {
   pageNumber += 1
 
-  if (pageNumber > 4) {
-    pageNumber = 1
+  if (pageNumber > pages.length - 1) {
+    pageNumber = 0
   }
 
   updateSection()
 }
 
+// function to decrease page number
 const previous = function () {
   pageNumber -= 1
 
-  if (pageNumber < 1) {
-    pageNumber = 4
+  if (pageNumber < 0) {
+    pageNumber = pages.length - 1
   }
 
   updateSection()
 }
 
 const updateSection = function () {
-  outputTag.innerHTML = `It worked ${pageNumber}`
+  outputTag.innerHTML = pages[pageNumber]
 }
 
 // event listener for next tag for output to middle paragraph
