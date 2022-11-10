e98355facc46a8bbd009ad251e1657d48a891579
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-11-10 13:52:36 +0000

    added logic for previous button/cleaned up code

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 let pageNumber = 0
 
 const nextTag = document.querySelector("div img.is-next")
+const prevTag = document.querySelector("div img.is-previous")
 const outputTag = document.querySelector("p")
 
-// added event listener to next tag for output to middle paragraph
-nextTag.addEventListener("click", function () {
-  pageNumber = pageNumber + 1
+const next = function () {
+  pageNumber += 1
+
+  if (pageNumber > 4) {
+    pageNumber = 1
+  }
+
+  updateSection()
+}
+
+const previous = function () {
+  pageNumber -= 1
+
+  if (pageNumber < 1) {
+    pageNumber = 4
+  }
+
+  updateSection()
+}
+
+const updateSection = function () {
   outputTag.innerHTML = `It worked ${pageNumber}`
+}
+
+// event listener for next tag for output to middle paragraph
+nextTag.addEventListener("click", function () {
+  next()
+})
+
+// event listener for previous tag for output to middle paragraph
+prevTag.addEventListener("click", function () {
+  previous()
 })
