196be6cda42f7b8f01123bb181c5913c355f0f2b
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-11-11 15:18:19 +0000

    made bg color and circle color dynamic

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ let pageNumber = 0
 
 // content for pages
 const pages = [
-  { copy: "a Londoner"},
-  { copy: "a foodie"},
-  { copy: "a JS developer"},
-  { copy: "looking for a job"},
-  { copy: "positive!"}
+  { copy: "a Londoner", bgColor: "#edc7a9", circleColor: "#3e78ed"},
+  { copy: "a foodie", bgColor: "#a1fffe", circleColor: "#e34a47"},
+  { copy: "a JS developer", bgColor: "#d3c7f3", circleColor: "#f7fe00"},
+  { copy: "looking for a job", bgColor: "#faffb8", circleColor: "#b472e6"},
+
 ]
 
 const nextTag = document.querySelector("div img.is-next")
 const prevTag = document.querySelector("div img.is-previous")
 const outputTag = document.querySelector("p")
+const circleTag = document.querySelector("div.slide_circle")
+const bodyTag = document.querySelector("body")
 
 // function to increase page number
 const next = function () {
@@ -38,6 +40,8 @@ const previous = function () {
 
 const updateSection = function () {
   outputTag.innerHTML = pages[pageNumber].copy
+  circleTag.style.backgroundColor = pages[pageNumber].circleColor
+  bodyTag.style.backgroundColor = pages[pageNumber].bgColor
 }
 
 // event listener for next tag for output to middle paragraph
